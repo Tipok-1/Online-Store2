be92ef27fdca93d8a2c633e52dd5c675dc2d133c
Timofei <tima.chaburko@mail.ru>
2023-03-29 17:54:16 +0300

    feat:add router

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,13 @@
 const Router = require('express');
 const router = new Router();
+const deviceRouter = require('./deviceRouter');
+const brandRouter = require('./brandRouter');
+const typeRouter = require('./typeRouter');
+const userRouter = require('./userRouter');
 
-router.use('/user',)
-router.use('/type',)
-router.use('/brand',)
-router.use('/device',)
+router.use('/user', userRouter)
+router.use('/type', typeRouter)
+router.use('/brand', brandRouter)
+router.use('/device', deviceRouter)
 
 module.exports = router;
