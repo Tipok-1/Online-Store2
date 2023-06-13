4f949da8f6ecba74d46f99f51b37d3bad67c591e
Timofei <tima.chaburko@mail.ru>
2023-06-13 21:44:45 +0300

    feat:add delete, update device function

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,6 +1,8 @@
 const uuid = require('uuid');
 const path = require('path');
+const fs = require('fs/promises');
 const {Device, DeviceInfo} = require('../models/models')
+const ApiError = require('../error/ApiError');
 
 class DeviceController {
     async create(req, res, next) {
@@ -13,12 +15,13 @@ class DeviceController {
             const device = await Device.create({name, price, brandId, typeId, img:fileName});
 
             if(info) {
+                info = JSON.parse(info);
                 info.forEach(i => {
                     DeviceInfo.create({
                         title: i.title,
                         description: i.description,
                         deviceId: device.id
-                    })
+                    }) //добавляем асинхронно без await 
                 });
             }
             return res.json(device);
@@ -61,11 +64,73 @@ class DeviceController {
                 include:[{model:DeviceInfo, as: 'info'}]
             })
             
-            return nres.json(device);
+            return res.json(device);
+        } catch(e) {
+            next(e);
+        }
+    }
+
+    async delete(req, res, next) {
+        try{
+            const {id} = req.params;
+            const device  = await Device.findOne({where:{id}})
+
+            DeviceInfo.destroy({where:{deviceId: device.id}}); //удаляем асинхронно без await 
+            const filePath = path.resolve(__dirname, '..', 'static', device.img);
+            fs.unlink(filePath); //удаляем асинхронно без await 
+            const deviceData  = await device.destroy();
+
+            return res.json(deviceData);
+        } catch(e) {
+            next(e);
+        }
+    }
+
+    async update(req, res, next) {
+        try{
+            const {id, ...changedField} = req.body;
+            const {img} = req.files;
+            const device = await Device.findOne({
+                where:{id},
+                include:[{model:DeviceInfo, as: 'info'}]
+            })
+            if(!device) {
+                throw ApiError.badRequest(`Продукта с ID - ${id} не существует`);
+            }
+            let fields = Object.keys(changedField);
+            if(img) {
+                const fileName = uuid.v4() +'.jpg';
+                img.mv(path.resolve(__dirname, '..', 'static', fileName));
+                const filePath = path.resolve(__dirname, '..', 'static', device.img);
+                fs.unlink(filePath); //удаляем асинхронно без await 
+                device.img = fileName;
+            }
+            for(let i = 0; i<fields.length; i++) {
+                if(fields[i] in device) {
+                    if(fields[i] == 'info') {
+                        const info = JSON.parse(changedField[fields[i]]);
+                        await Promise.all(
+                            info.map( i=> DeviceInfo.update({title:i.title, description:i.description},{where: {id:i.id}}))
+                        )
+                    }
+                    else {
+                        device[fields[i]] = changedField[fields[i]];
+                    }
+                }
+            }
+
+            await device.save();
+
+            let p = await Device.findOne({
+                where:{id},
+                include:[{model:DeviceInfo, as: 'info'}]
+            })
+            return res.json(p);
         } catch(e) {
             next(e);
         }
     }
 }
+
 
 module.exports = new DeviceController();
--- server/routes/deviceRouter.js
+++ server/routes/deviceRouter.js
@@ -4,7 +4,9 @@ const deviceController = require('../controllers/deviceController');
 const checkRole = require('../middleware/CheckRoleMiddlware');
 
 router.post('/', checkRole('ADMIN'), deviceController.create);
-router.get('/',deviceController.getAll);
-router.get('/:id',deviceController.getOne);
+router.put('/', checkRole('ADMIN'), deviceController.update);
+router.delete('/:id', checkRole('ADMIN'), deviceController.delete);
+router.get('/', deviceController.getAll);
+router.get('/:id', deviceController.getOne);
 
 module.exports = router;
