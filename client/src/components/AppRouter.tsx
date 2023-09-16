b4c233092377005711141254d831bd20447d24cc
Timofei <tima.chaburko@mail.ru>
2023-09-16 16:17:44 +0300

    feat:client - change routes

--- client/src/components/AppRouter.tsx
+++ client/src/components/AppRouter.tsx
@@ -1,10 +1,23 @@
-import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import Navbar from './Navbar/Navbar';
 import { authRoutes, publicRoutes } from '../routes';
+import { INavbar } from './Navbar/Navbar';
+import Footer from './Footer/Footer';
 
-const AppRouter = () => {
-    let isAuth = false;
-    let router = createBrowserRouter((isAuth?[...authRoutes, ...publicRoutes]:publicRoutes));
-    return (<RouterProvider router={router} />);
+const AppLayout = (obj: INavbar) => (
+  <>
+    <Navbar title={obj.title} loading={obj.loading} />
+    <Outlet />
+    <Footer />
+  </>
+);
+const AppRouter = (obj: INavbar) => {
+  let isAuth = true;
+  let router = createBrowserRouter([{
+    element: <AppLayout title={obj.title} loading={obj.loading} />,
+    children: (isAuth ? [...authRoutes, ...publicRoutes] : publicRoutes),
+  }])
+  return (<RouterProvider router={router} />);
 };
 
 export default AppRouter;
